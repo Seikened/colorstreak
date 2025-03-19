674266e9dcc8fb076fc88614b145f63d37678a94
Fernando Leon Franco <fernanlee2131@gmail.com>
2025-03-19 12:00:25 -0600

    libreria

---
+++ test.py
@@ -0,0 +1,8 @@
+from colorlogterminal import log
+
+
+log.debug("This is a debug message")
+log.info("This is an info message")
+log.warning("This is a warning message")
+log.error("This is an error message")
+log.deprecated("This is a deprecated message")
