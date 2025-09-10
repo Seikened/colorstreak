6e4697ca1ce1f5776c7edc4c1fee4bd4701300fd
Fernando Leon Franco <fernanlee2131@gmail.com>
2025-09-10 00:07:47 -0600

    chore: actualizar versión a 2.0.0 en setup.py y tag.sh - Cambiada la versión de '1.0.0' a '2.0.0' en setup.py - Actualizada la versión en tag.sh a 'v2.0.0'

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='colorstreak',
-    version='1.0.0',
+    version='2.0.0',
     description='Terminal color formatting for Python, with logging capabilities',
     author='Fernando Leon Franco',
     packages=find_packages(),
